b8a1060f0eb6cf80145d1614fe3685802d0218ba
Oluwadara <maxegunjobi@gmail.com>
2024-07-27 18:18:28 +0100

    chore/setup-node-typescript-environment
    chore(setup): Set up Node.js and TypeScript environment

---
+++ src/swagger.ts
@@ -0,0 +1,23 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express API with Swagger',
+      version: '1.0.0',
+      description: 'A simple CRUD API application made with Express and documented with Swagger',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts'], // files containing annotations as above
+};
+
+const specs = swaggerJsdoc(options);
+
+export { swaggerUi, specs };
